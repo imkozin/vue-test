89095325d8807cd0740049080729cd09ef723aa0
imkozin <ivan.kozin@hotmail.com>
2024-03-16 23:54:13 +0300

    displayed data

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <pre>{{ data }}</pre> -->
+  <div v-for="variant in data" :key="variant.id">
+  <h3>{{ variant.alleleName }}</h3>
+      <p>Significance: {{ variant.significance }}</p>
+      <p>Genotype: {{ variant.genotype }}</p>
+      <ul>
+        <p>HGVS Notation:</p>
+        <li v-for="notation in variant.hgvs">{{ notation }}</li>
+      </ul>
+      <p>External Sources:</p>
+      <ul>
+        <li v-for="source in variant.externalSourceEntries" :key="source.id">
+          <a :href="source.link" target="_blank">{{ source.database.name }}</a> - Significance: {{ source.significance }}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import userData from '../assets/variants.json'
+
+const data = computed(() => {
+    console.log(userData);
+  return userData.variants
+})
+</script>
+
+<style scoped></style>
