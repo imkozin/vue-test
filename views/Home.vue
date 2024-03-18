be254625c5f55c0188a9bf8d9912428fadc91a11
imkozin <ivan.kozin@hotmail.com>
2024-03-18 21:25:39 +0300

    added filtering by genotype

---
+++ views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main">
+    <DataTable :items="items" :data="data"/>
+  </div>
+</template>
+
+<script setup>
+import DataTable from '@/components/DataTable.vue';
+import { computed } from 'vue'
+import userData from '../assets/variants.json'
+
+const items = [
+  {
+    id: 1,
+    title: "Variant Name"
+  },
+  {
+    id: 2,
+    title: "Significance"
+  },
+  {
+    id: 3,
+    title: "Genotype"
+  },
+  {
+    id: 4,
+    title: "HGVS Notation"
+  },
+  {
+    id: 5,
+    title: "External Sources"
+  }
+]
+
+const data = computed(() => {
+  console.log(userData)
+  return userData.variants
+})
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
